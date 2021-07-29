5ce19acea380c79ebee21387e32937626c6efa87
Lavinya Correia <lavinya.correia01@gmail.com>
2021-07-29 13:21:22 -0300

    Plano de fundo e texto na tela

--- main.py
+++ main.py
@@ -30,13 +30,31 @@ BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background
 def main():
     run = True
     FPS = 60
+    level = 1
+    lives = 5
+    main_font = pygame.font.SysFont("comicsans", 50)
+
     clock = pygame.time.Clock()
+
+    #Desenho do jogo
+    def redraw_window():
+        WIN.blit(BG, (0,0))
+        #Texto
+        lives_label = main_font.render(f"Lives: {lives}", 1, (255,255,255))
+        level_label = main_font.render(f"Level: {level}", 1, (255,255,255))
+        WIN.blit(lives_label, (10, 10))
+        WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
+
+        pygame.display.update()
 
     while run:
         clock.tick(FPS)
+        redraw_window()
 
+        #Fechar o programa
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+           
 
 main()
