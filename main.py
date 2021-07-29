f5b8499a01d52f2cff142073cdd39e901d6aeb41
Lavinya Correia <lavinya.correia01@gmail.com>
2021-07-29 17:02:03 -0300

    instancia do objeto do jogador

--- main.py
+++ main.py
@@ -38,8 +38,19 @@ class Item:
         self.cool_down_counter = 0
     
     def draw(self, window):
-        #desenho de um retângulo para teste 
-        pygame.draw.rect(window, (255, 0, 0), (self.x, self.y, 50, 50))
+        window.blit(self.ship_img, (self.x, self.y))
+
+
+#Player - Herdando a classe Item
+class Player(Item):
+    def __init__(self, x, y, health=100):
+        super().__init__(x, y, health)
+        self.ship_img = YELLOW_SHIP
+        self.laser_img = YELLOW_LASER
+        self.mask = pygame.mask.from_surface(self.ship_img)
+        self.max_health=health
+
+
 
 
 def main():
@@ -50,7 +61,7 @@ def main():
     main_font = pygame.font.SysFont("comicsans", 50)
     velocidade_jogador = 5
 
-    ship = Item(300,650)
+    player = Player(300,650)
 
     clock = pygame.time.Clock()
 
@@ -58,12 +69,12 @@ def main():
     def redraw_window():
         WIN.blit(BG, (0,0))
         #Texto
-        lives_label = main_font.render(f"Lives: {lives}", 1, (255,255,255))
+        lives_label = main_font.render(f"Vidas: {lives}", 1, (255,255,255))
         level_label = main_font.render(f"Level: {level}", 1, (255,255,255))
         WIN.blit(lives_label, (10, 10))
         WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
 
-        ship.draw(WIN)
+        player.draw(WIN)
         pygame.display.update()
 
     while run:
@@ -77,13 +88,13 @@ def main():
         
         #Ação dos botões
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT] and ship.x - velocidade_jogador > 0: #esquerda
-            ship.x -= velocidade_jogador
-        if keys[pygame.K_RIGHT] and ship.x + velocidade_jogador < WIDTH: #direita
-            ship.x += velocidade_jogador
-        if keys[pygame.K_UP] and ship.y - velocidade_jogador > 0: #para cima
-            ship.y -= velocidade_jogador
-        if keys[pygame.K_DOWN] and ship.y + velocidade_jogador <HEIGHT: #para baixo
-            ship.y += velocidade_jogador
+        if keys[pygame.K_LEFT] and player.x - velocidade_jogador > 0: #esquerda
+            player.x -= velocidade_jogador
+        if keys[pygame.K_RIGHT] and player.x + velocidade_jogador + 50< WIDTH: #direita
+            player.x += velocidade_jogador
+        if keys[pygame.K_UP] and player.y - velocidade_jogador > 0: #para cima
+            player.y -= velocidade_jogador
+        if keys[pygame.K_DOWN] and player.y + velocidade_jogador + 50<HEIGHT: #para baixo
+            player.y += velocidade_jogador
         
 main()
